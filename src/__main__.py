8a0e39b01a8238798a6c1359059a4c9fe9e8b484
Asncodes-80 <asn80.asn@hotmail.com>
2024-03-20 18:01:58 +0330

    complete connections

--- src/__main__.py
+++ src/__main__.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import _cm
+from matplotlib import colormaps
 
 
 def normalized_data(raw_data):
@@ -18,7 +18,27 @@ def normalized_data(raw_data):
     return df[["preferredName_A", "preferredName_B", "score"]]
 
 
-def draw_graph(data, proteins):
+def graph_info(G, proteins):
+    # Network info about edges (Interactions) and Nodes (Proteins)
+    print(f"Number of nodes: {G.number_of_nodes()}")
+    print(f"Number of edges: {G.number_of_edges()}")
+    print("Avg degree of each protein:")
+    for protein in proteins:
+        print(
+            f"Protein {protein}: {round(nx.average_neighbor_degree(G).get(protein))} - {G.degree(protein)}"
+        )
+
+
+def rescale(l, new_min, new_max):
+    # Returns degree of each |V|
+    arr = list(l)
+    return [
+        (x - min(arr)) / (max(arr) - min(arr)) * (new_max - new_min) + new_min
+        for x in arr
+    ]
+
+
+def draw_graph(data, proteins, option="normal", mst=False):
     # A graph instance to show interactions of Protein 'A' to Protein 'B'
     G = nx.Graph(name="Protein Interactions Graph")
     interactions = np.array(normalized_data(data))
@@ -32,18 +52,58 @@ def draw_graph(data, proteins):
         # Connect nodes (Protein A and B) with simple weighted |E|
         G.add_weighted_edges_from([(protein_a, protein_b, weight)])
 
-    # Network info about edges (Interactions) and Nodes (Proteins)
-    print(f"Number of nodes: {G.number_of_nodes()}")
-    print(f"Number of edges: {G.number_of_edges()}")
-    print("Avg degree of each protein:")
-    for protein in proteins:
-        print(f"Protein {protein}: {nx.average_neighbor_degree(G).get(protein)}")
+    graph_info(G, proteins)
 
-    pos = nx.spring_layout(G)
-    plt.figure(figsize=(11, 11))
-    nx.draw_networkx(G)
-    plt.axis("off")
-    plt.show()
+    if option == "normal":
+        pos = nx.spring_layout(G)
+        plt.figure(figsize=(11, 11))
+        nx.draw_networkx(G)
+        plt.axis("off")
+        plt.show()
+    elif option == "betweenness_centrality":
+        # Matplotlib plasma colormap
+        graph_colormap = colormaps.get_cmap("plasma")
+        # Each node color config to varies with degree
+        node_color = rescale([G.degree(v) for v in G], 0.0, 0.9)
+        node_color = [graph_colormap(i) for i in node_color]
+        # Node size varies with betweenness centrality - map to range [10, 100]
+        bc = nx.betweenness_centrality(G)
+        node_size = rescale([v for v in bc.values()], 1500, 7000)
+        # Edge width shows 1-weight to convert cost back to strength of
+        # interaction
+        edge_width = rescale([float(G[u][v]["weight"]) for u, v in G.edges], 0.1, 4)
+        # Edge color also demonstrates weight
+        edge_color = rescale([float(G[u][v]["weight"]) for v, u in G.edges], 0.1, 1)
+        edge_color = [graph_colormap(i) for i in edge_color]
+
+        pos = None
+        T = None
+
+        if mst == False:
+            pos = nx.spring_layout(G)
+        else:
+            T = nx.minimum_spanning_tree(G)
+            pos = nx.spring_layout(T)
+
+        figure = plt.figure(figsize=(19, 6), facecolor=[0.7, 0.7, 0.7, 0.1])
+        figure.canvas.manager.set_window_title(option + f"_{str(mst).lower()}")
+
+        nx.draw_networkx(
+            G if mst == False else T,
+            pos=pos,
+            with_labels=True,
+            node_color=node_color,
+            node_size=node_size,
+            edge_color=edge_color,
+            width=edge_width,
+            font_color="white",
+            font_weight="bold",
+            font_size="9",
+        )
+        plt.axis("off")
+        plt.show()
+    else:
+        print("Option type error")
 
 
 def main():
@@ -78,8 +138,8 @@ def main():
     url = f"https://string-db.org/api/tsv/network?identifiers={proteins}&species=9606"
     try:
         r = requests.get(url)
-        draw_graph(r, proteins_list)
-    except requests.exceptions.RequestException as e:
+        draw_graph(r, proteins_list, option="betweenness_centrality", mst=False)
+    except requests.exceptions.RequestException as _:
         raise SystemExit("Network error, Please check your internet connection.")
 
 
